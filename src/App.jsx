e8c4d7416d79dd216eeae048c1c782f2da59d311
MariaAncona <maria.ancona18@gmail.com>
2025-02-15 11:56:45 -0600

    segunda entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/layouts/navbar/Navbar"
-import ItemListContainer from "./components/pages/ItemListContainer";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
 
 function App() {
---
+++ src/products.js
@@ -0,0 +1,39 @@
+export const products = [
+    {
+        id: "1",
+        title: "Monstera",
+        price: 300,
+        stock: 50,
+        category: "plantas",
+        description: "Arbusto de interior y exterior",
+        imageUrl: ""
+    },
+    {
+        id: "2",
+        title: "Hule",
+        price: 500,
+        stock: 10,
+        category: "plantas",
+        description: "Mediana altura de interior",
+        imageUrl: ""
+    },
+    {
+        id: "3",
+        title: "Palmera",
+        price: 1500,
+        stock: 5,
+        category: "Árbol",
+        description: "Exterior 2 a 5 metros de altura",
+        imageUrl: ""
+    },
+
+    {
+        id: "4",
+        title: "Naranjo",
+        price: 2500,
+        stock: 3,
+        category: "Árbol",
+        description: "Exterior 2 a 3.5 metros de altura",
+        imageUrl: ""
+    },
+]
